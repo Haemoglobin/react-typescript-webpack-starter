3974d40f56c4d46b85d837502c4707c471b88856
Haemoglobin <github-hgkey@snkmail.com>
2015-12-27 20:50:37 +0000

    Add React and Webpack

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {  
+  entry: './src/app.tsx',
+  output: {
+    filename: 'bundle.js',
+    path: 'dist', 
+    hash: true
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['', '.webpack.js', '.web.js', '.ts', '.js']
+  },
+  plugins: [
+    // new webpack.optimize.UglifyJsPlugin(),
+    new HtmlWebpackPlugin({
+        title: 'React Typescript Webpack Starter',
+        template: './src/index.html', 
+        inject: 'body'
+    })
+  ],
+  module: {
+    loaders: [
+      { test: /\.tsx?$/, loader: 'ts-loader' }
+    ]
+  }
+}
